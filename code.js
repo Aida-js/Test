77184217333d93a744168bb4c5244476e2268222
george <goshan.min.js@icloud.com>
2019-08-05 00:26:09 +0300

    добавила поле для поиска

---
+++ code.js
@@ -0,0 +1,11 @@
+function getArr () {
+    return [
+        {
+            id: "1",
+            text: 'Я УМЕЮ! Я МОГУ!',
+            needAlert: false,
+            show: true,
+            isShowed: false,
+        },
+    ];
+}
